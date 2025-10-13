72e293a5199c4645b0db55756d509a95a35b3bda
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2025-10-13 15:34:22 +0300

    Create CheckingAccount.cs

---
+++ MiniBank Console/MiniBank Console/Models/CheckingAccount.cs
@@ -0,0 +1,24 @@
+﻿using MiniBank_Console.Models.Interfaces;
+
+namespace MiniBank_Console.Models;
+
+public class CheckingAccount : BankAccount, IOverdraftPolicy
+{
+    public CheckingAccount(string owner, decimal balance) : base(owner, balance)
+    {
+        Log($"Checking account (ID: {Id}) created for {owner} with initial balance {balance:C}");
+    }
+
+    public decimal OverdraftLimit => 200;
+
+    protected override bool CanWithdraw(decimal amount, out string? error)
+    {
+        if(Balance - amount < -OverdraftLimit)
+        {
+            error = $"Withdrawal exceeds overdraft limit of {OverdraftLimit:C}.";
+            return false;
+        }
+        error = null;
+        return true;
+    }
+}
