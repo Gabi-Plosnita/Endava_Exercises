c226a1100dcd79a2a247f84da7ee27c8163e3395
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2025-10-13 17:09:02 +0300

    Create IBankAccountService.cs

---
+++ MiniBank Console/MiniBank Console/Services/Interfaces/IBankAccountService.cs
@@ -0,0 +1,9 @@
+﻿using MiniBank_Console.Models;
+
+namespace MiniBank_Console.Services.Interfaces;
+
+public interface IBankAccountService
+{
+    IReadOnlyList<BankAccount> Accounts { get; }
+    bool CreateAccount(string owner, decimal balance, AccountType accountType, out string? error);
+}
