94d0b2b058b1c2003ccbb81ac92ce9ec24e7585a
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2025-10-13 14:19:17 +0300

    Create BankAccount.cs

---
+++ MiniBank Console/MiniBank Console/BankAccount.cs
@@ -0,0 +1,19 @@
+﻿namespace MiniBank_Console;
+
+public abstract class BankAccount
+{
+    private static int nextId = 1;
+    public int Id { get; }
+    public string Owner { get; protected set; }
+    public decimal Balance { get; protected set; }
+
+    public BankAccount(string owner, decimal balance)
+    {
+        Id = nextId++;
+        Owner = owner;
+        Balance = balance;
+    }
+
+    public abstract void Deposit(decimal amount);
+    public abstract bool Withdraw(decimal amount, out string? error);
+}
