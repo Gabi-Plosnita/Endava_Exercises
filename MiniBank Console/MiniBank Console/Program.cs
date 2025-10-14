3a1bd631bfab294e32eb3ace3842c1c22e5be18a
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2025-10-14 11:15:49 +0300

    Call SeedData in program.cs

--- MiniBank Console/MiniBank Console/Program.cs
+++ MiniBank Console/MiniBank Console/Program.cs
@@ -3,5 +3,6 @@ using MiniBank_Console.Services;
 using MiniBank_Console.Services.Interfaces;
 
 IBankAccountService bankAccountService = new BankAccountService();
+bankAccountService.SeedData();
 var app = new MiniBankApp(bankAccountService);
 app.Run();
