277f959219c6e11d85eb9809d6defa673a61265b
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2025-10-14 16:01:00 +0300

    Set CultureInfo to Ro

--- MiniBank Console/MiniBank Console/Program.cs
+++ MiniBank Console/MiniBank Console/Program.cs
@@ -1,7 +1,10 @@
 ﻿using MiniBank_Console;
 using MiniBank_Console.Services;
 using MiniBank_Console.Services.Interfaces;
+using System.Globalization;
 
+Thread.CurrentThread.CurrentCulture = new CultureInfo("ro-RO");
+Thread.CurrentThread.CurrentUICulture = new CultureInfo("ro-RO");
 IBankAccountService bankAccountService = new BankAccountService();
 bankAccountService.SeedData();
 var app = new MiniBankApp(bankAccountService);
