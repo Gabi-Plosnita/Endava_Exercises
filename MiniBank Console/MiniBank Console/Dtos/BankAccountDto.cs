748e5268b55ef9ba97dcf9b9286689ce160a34bd
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2025-10-14 17:56:17 +0300

    Create BankAccountDto.cs

---
+++ MiniBank Console/MiniBank Console/Dtos/BankAccountDto.cs
@@ -0,0 +1,13 @@
+﻿using MiniBank_Console.Models;
+
+namespace MiniBank_Console.Dtos;
+
+public class BankAccountDto
+{
+    public AccountType Type { get; set; }
+    public int Id { get; set; }
+    public string Owner { get; set; } = string.Empty;
+    public decimal Balance { get; set; }
+    public List<string> OperationLog { get; set; } = [];
+    public DateTime EndDate { get; set; }
+}
