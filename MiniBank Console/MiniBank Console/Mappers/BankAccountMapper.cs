9263ba205359924eebae19a33760cea0b25039c2
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2025-10-15 11:01:02 +0300

    Create BankAccountMapper.cs

---
+++ MiniBank Console/MiniBank Console/Mappers/BankAccountMapper.cs
@@ -0,0 +1,32 @@
+﻿using MiniBank_Console.Dtos;
+using MiniBank_Console.Models;
+
+namespace MiniBank_Console.Mappers;
+
+public static class BankAccountMapper
+{
+    public static BankAccountDto ToBankAccountDto(this BankAccount bankAccount) => new()
+    {
+        Type = bankAccount switch
+        {
+            CheckingAccount => AccountType.Checking,
+            SavingsAccount => AccountType.Savings,
+            LoanAccount => AccountType.Loan,
+            FixedDepositAccount => AccountType.FixedDepositAccount,
+            _ => throw new InvalidOperationException($"Unknown account type: {bankAccount.GetType().Name}")
+        },
+        Id = bankAccount.Id,
+        Owner = bankAccount.Owner,
+        Balance = bankAccount.Balance,
+        OperationLog = bankAccount.OperationLog.ToList()
+    };
+
+    public static BankAccount ToBankAccount(this BankAccountDto dto) => dto.Type switch
+    {
+        AccountType.Checking => new CheckingAccount(dto.Id, dto.Owner, dto.Balance, dto.OperationLog),
+        AccountType.Savings => new SavingsAccount(dto.Id, dto.Owner, dto.Balance, dto.OperationLog),
+        AccountType.Loan => new LoanAccount(dto.Id, dto.Owner, dto.Balance, dto.OperationLog),
+        AccountType.FixedDepositAccount => new FixedDepositAccount(dto.Id, dto.Owner, dto.Balance, dto.EndDate, dto.OperationLog),
+        _ => throw new InvalidOperationException($"Unknown account type '{dto.Type}'.")
+    };
+}
