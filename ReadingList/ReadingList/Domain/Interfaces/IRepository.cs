c8704b83804c6583b79bb5030374717570111828
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2025-10-23 10:01:43 +0300

    Create IRepository.cs

---
+++ ReadingList/ReadingList/Domain/Interfaces/IRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace ReadingList.Domain;
+
+public interface IRepository<T, TKey> where TKey : notnull
+{
+    bool Add(T item);              
+    bool Upsert(T item);              
+    bool Contains(TKey key);
+    bool TryGet(TKey key, out T? item);
+    IEnumerable<T> All();
+}
+
