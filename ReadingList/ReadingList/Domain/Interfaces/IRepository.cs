b7248f26c65024c1dbf9d8643cd1f06cf54ef5e8
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2025-10-23 10:33:18 +0300

    Update IRepository.cs

--- ReadingList/ReadingList/Domain/Interfaces/IRepository.cs
+++ ReadingList/ReadingList/Domain/Interfaces/IRepository.cs
@@ -3,7 +3,7 @@
 public interface IRepository<T, TKey> where TKey : notnull
 {
     bool Add(T item);              
-    bool Upsert(T item);              
+    bool Update(T item);              
     bool Contains(TKey key);
     bool TryGet(TKey key, out T? item);
     IEnumerable<T> All();
