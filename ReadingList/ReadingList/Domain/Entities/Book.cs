2ac6724d267d530f6bdb24f0e3f07a399f8a0673
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2025-10-23 11:54:55 +0300

    Create Book.cs

---
+++ ReadingList/ReadingList/Domain/Entities/Book.cs
@@ -0,0 +1,13 @@
+﻿namespace ReadingList.Domain;
+
+public class Book
+{
+    public required int Id { get; set; }
+    public required string Title { get; set; } = string.Empty;
+    public required string Author { get; set; } = string.Empty;
+    public required int Year { get; set; }
+    public required int Pages { get; set; }
+    public required Genre Genre { get; set; }
+    public required bool Finished { get; set; }
+    public required double Rating { get; set; }
+}
