f721588bc9a89d1404b43c7dbf395be96ecd09a6
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2025-10-23 10:58:24 +0300

    Create Result.cs

---
+++ ReadingList/ReadingList/Domain/Results/Result.cs
@@ -0,0 +1,59 @@
+﻿namespace ReadingList.Domain;
+
+public class Result
+{
+    public bool IsSuccessful => Errors.Count == 0;
+    public bool IsFailure => !IsSuccessful;
+    public List<string> Errors { get; } = new();
+
+    public Result()
+    {
+    }
+
+    public Result(IEnumerable<string> errors)
+    {
+        if (errors != null)
+        {
+            Errors.AddRange(errors);
+        }
+    }
+
+    public void AddError(string error)
+    {
+        if (!string.IsNullOrWhiteSpace(error))
+        {
+            Errors.Add(error);
+        }
+    }
+
+    public void AddErrors(IEnumerable<string> errors)
+    {
+        if (errors != null)
+        {
+            Errors.AddRange(errors);
+        }
+    }
+}
+
+public class Result<T> : Result
+{
+    public T? Value { get; }
+
+    public Result()
+    {
+    }
+
+    public Result(T value)
+    {
+        Value = value;
+    }
+
+    public Result(T value, IEnumerable<string> errors) : base(errors)
+    {
+        Value = value;
+    }
+
+    public Result(IEnumerable<string> errors) : base(errors)
+    {
+    }
+}
