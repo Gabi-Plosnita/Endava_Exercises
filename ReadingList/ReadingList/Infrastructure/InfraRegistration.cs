284e46499a6eff9ac39c42f5c6f522b7d6e5dafe
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2025-10-24 11:07:37 +0300

    Add infrastructure DI registration class
    Introduces InfraRegistration with AddInfrastructure extension method for dependency injection. Registers IImportService with BookImportService implementation.

---
+++ ReadingList/ReadingList/Infrastructure/InfraRegistration.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using ReadingList.Domain;
+
+namespace ReadingList.Infrastructure;
+
+public static class InfraRegistration
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+    {
+        //services.AddScoped<IRepository<Book, int>, InMemoryRepository<Book, int>();
+        services.AddScoped<IImportService, BookImportService>();
+        return services;
+    }
+}
