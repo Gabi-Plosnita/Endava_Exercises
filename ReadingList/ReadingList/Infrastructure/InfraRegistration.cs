5cbe0c0ebcb43a21cd4b948dee1193b12bf1a9cf
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2025-10-24 11:18:12 +0300

    Update InfraRegistration.cs

--- ReadingList/ReadingList/Infrastructure/InfraRegistration.cs
+++ ReadingList/ReadingList/Infrastructure/InfraRegistration.cs
@@ -7,7 +7,7 @@ public static class InfraRegistration
 {
     public static IServiceCollection AddInfrastructure(this IServiceCollection services)
     {
-        //services.AddScoped<IRepository<Book, int>, InMemoryRepository<Book, int>();
+        services.AddScoped<IRepository<Book, int>>(sp => new InMemoryRepository<Book, int>(book => book.Id));
         services.AddScoped<IImportService, BookImportService>();
         return services;
     }
