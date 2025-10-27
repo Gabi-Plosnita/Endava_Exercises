89aa730b7f1bcb564faf01999d63b77784e8dc51
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2025-10-27 15:04:33 +0200

    Add help command constants
    Introduced HelpCommandKeyword and HelpCommandSummary to support a help command. Also reordered ExistCommandKeyword for consistency.

--- ReadingList/ReadingList/App/Constants.cs
+++ ReadingList/ReadingList/App/Constants.cs
@@ -3,9 +3,12 @@
 public static class Constants
 {
     public const string LoggingFilePath = "Logs/log.txt";
-    public const string ExistCommandKeyword = "exit";
 
     // Commands //
+    public const string ExistCommandKeyword = "exit";
     public const string ImportCommandKeyword = "import";
     public const string ImportCommandSummary = "import <file1.csv> [file2.csv ...] — imports one or more CSV files in parallel.";
+    public const string HelpCommandKeyword = "help";
+    public const string HelpCommandSummary = "help — display all available commands.";
+
 }
