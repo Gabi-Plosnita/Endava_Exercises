3577a85ac062668ef3a60d686812a2b54963399d
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2025-10-27 15:37:27 +0200

    Add ListAllCommand to list all books
    Introduces the ListAllCommand class, which implements ICommand and lists all books from the repository when executed.

---
+++ ReadingList/ReadingList/App/Commands/ListAllCommand.cs
@@ -0,0 +1,21 @@
+﻿
+using ReadingList.Domain;
+
+namespace ReadingList.App;
+
+public class ListAllCommand(IRepository<Book, int> _repository) : ICommand
+{
+    public string Keyword => "list_all";
+
+    public string Summary => "Lists all books";
+
+    public Task ExecuteAsync(string[] args, CancellationToken ct)
+    {
+        var books = _repository.All();
+        foreach (var book in books)
+        {
+            Console.WriteLine(book);
+        }
+        return Task.CompletedTask;
+    }
+}
