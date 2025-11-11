0f6abc6f81f0c540a9918e75a899f8bef29d23cb
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2025-11-11 17:43:01 +0200

    Add SimpleOrderEventPublisher implementation
    Introduces SimpleOrderEventPublisher to manage order event subscribers and publish OrderPlaced events. Provides methods to subscribe and unsubscribe event subscribers.

---
+++ CafeConsole/Cafe.Application/Services/SimpleOrderEventPublisher.cs
@@ -0,0 +1,26 @@
+﻿using Cafe.Domain;
+
+namespace Cafe.Application;
+
+public class SimpleOrderEventPublisher : IOrderEventPublisher
+{
+    private readonly List<IOrderEventSubscriber> _subscribers = new();
+
+    public void PublishOrderPlaced(OrderPlaced orderPlaced)
+    {
+        foreach (var subscriber in _subscribers)
+        {
+            subscriber.OnOrderPlaced(orderPlaced);
+        }
+    }
+
+    public void Subscribe(IOrderEventSubscriber subscriber)
+    {
+        _subscribers.Add(subscriber);
+    }
+
+    public void Unsubscribe(IOrderEventSubscriber subscriber)
+    {
+        _subscribers.Remove(subscriber);
+    }
+}
