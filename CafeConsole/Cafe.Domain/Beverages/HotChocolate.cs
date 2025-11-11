fb806a01aa64074396ef2f194543341eac8c26f6
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2025-11-11 11:50:22 +0200

    Add HotChocolate beverage class
    Introduces the HotChocolate class implementing IBeverage with properties for name and cost, and methods for description and cost retrieval.

---
+++ CafeConsole/Cafe.Domain/Beverages/HotChocolate.cs
@@ -0,0 +1,19 @@
+﻿namespace Cafe.Domain;
+
+public class HotChocolate : IBeverage
+{
+    private string _name;
+    private decimal _cost;
+
+    public HotChocolate(string name, decimal cost)
+    {
+        _name = name;
+        _cost = cost;
+    }
+
+    public string Name => _name;
+
+    public decimal Cost() => _cost;
+
+    public string Describe() => Name;
+}
