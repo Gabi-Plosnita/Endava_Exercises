2efb16e7062d25b1a86461ae8a1687824b8dbb06
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2025-11-11 11:01:08 +0200

    Add Espresso beverage implementation
    Introduces the Espresso class implementing IBeverage with properties for name and cost, and methods for description and cost retrieval.

---
+++ CafeConsole/Cafe.Domain/Beverages/Espresso.cs
@@ -0,0 +1,19 @@
+﻿namespace Cafe.Domain;
+
+public class Espresso : IBeverage
+{
+    private string _name;
+    private decimal _cost;
+
+    public Espresso(string name, decimal cost)
+    {
+        _name = name;
+        _cost = cost;
+    }
+
+    public string Name => _name;
+
+    public decimal Cost() => _cost;
+
+    public string Describe() => Name;
+}
