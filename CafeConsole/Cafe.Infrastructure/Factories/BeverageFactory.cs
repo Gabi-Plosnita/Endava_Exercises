34c243b5b84a59ae07c88120dc20782285d97191
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2025-11-11 15:04:45 +0200

    Create BeverageFactory.cs

---
+++ CafeConsole/Cafe.Infrastructure/Factories/BeverageFactory.cs
@@ -0,0 +1,26 @@
+﻿using Cafe.Domain;
+
+namespace Cafe.Infrastructure;
+
+public class BeverageFactory : IBeverageFactory
+{
+    private readonly Dictionary<string, Func<IBeverage>> _registry;
+
+    public BeverageFactory(Dictionary<string, Func<IBeverage>> registry)
+    {
+        _registry = registry;
+    }
+
+    public IEnumerable<string> Keys => _registry.Keys;
+
+    public IBeverage Create(string key)
+    {
+        if (!_registry.TryGetValue(key, out var ctor))
+        {
+            throw new ArgumentException($"Unknown beverage '{key}'");
+        }
+
+        return ctor();
+    }
+}
+
