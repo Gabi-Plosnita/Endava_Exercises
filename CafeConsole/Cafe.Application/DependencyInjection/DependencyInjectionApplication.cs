0d07526b53cf9ed6c6bc4c99f7ddee7fa651c100
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2025-11-13 16:46:04 +0200

    Add DI setup for application services
    Introduced DependencyInjectionApplication.cs to register application services and strategies with Microsoft.Extensions.DependencyInjection. Updated project file to include the required dependency injection package.

---
+++ CafeConsole/Cafe.Application/DependencyInjection/DependencyInjectionApplication.cs
@@ -0,0 +1,19 @@
+﻿using Cafe.Domain;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Cafe.Application;
+
+public static class DependencyInjectionApplication
+{
+    public static IServiceCollection RegisterApplication(this IServiceCollection services)
+    {
+        services.AddSingleton<IPricingStrategy, RegularPricing>();
+        services.AddSingleton<IPricingStrategy, HappyHourPricing>();
+        services.AddSingleton<IPricingStrategyProvider, InMemoryPricingStrategyProvider>();
+        services.AddSingleton<IOrderEventPublisher, SimpleOrderEventPublisher>();
+
+        services.AddSingleton<IOrderService, OrderService>();
+
+        return services;
+    }
+}
