b704aafd6390013fc5440b7bdc38718044f3a186
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2025-11-13 21:13:21 +0200

    Add TeaRegistration for beverage factory
    Introduces the TeaRegistration class implementing IBeverageRegistration, enabling creation and validation of Tea beverages within the infrastructure layer.

---
+++ CafeConsole/Cafe.Infrastructure/Factories/Beverage/TeaRegistration.cs
@@ -0,0 +1,34 @@
+﻿using Cafe.Domain;
+
+namespace Cafe.Infrastructure;
+
+public class TeaRegistration : IBeverageRegistration
+{
+    private readonly string _name;
+    private readonly decimal _baseCost;
+
+    public TeaRegistration(string name, decimal baseCost)
+    {
+        _name = name;
+        _baseCost = baseCost;
+    }
+
+    public string Key => _name.ToLower();
+
+
+    public Result<IBeverage> Create()
+    {
+        var result = new Result<IBeverage>();
+        var tea = new Tea(_name, _baseCost);
+        var validationResult = tea.Validate();
+
+        if (validationResult.IsFailure)
+        {
+            result.AddErrors(validationResult.Errors);
+            return result;
+        }
+
+        result.Value = tea;
+        return result;
+    }
+}
