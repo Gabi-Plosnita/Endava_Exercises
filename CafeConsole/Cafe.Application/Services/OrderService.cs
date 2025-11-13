6911289c83b2f9a1df5b39c22bbf23b7f1d7afe2
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2025-11-13 10:49:47 +0200

    Add OrderService implementation
    Introduces the OrderService class, implementing order placement logic using pricing strategies, beverage creation, add-on decoration, and event publishing. This service coordinates dependencies and error handling for processing orders.

---
+++ CafeConsole/Cafe.Application/Services/OrderService.cs
@@ -0,0 +1,57 @@
+﻿using Cafe.Domain;
+
+namespace Cafe.Application;
+
+public class OrderService : IOrderService
+{
+    private readonly IPricingStrategyProvider _pricingStrategyProvider;
+    private readonly IBeverageFactory _beverageFactory;
+    private readonly IAddOnDecoratorFactory _addOnDecoratorFactory;
+    private readonly IOrderEventPublisher _orderEventPublisher;
+
+    public OrderService(
+        IPricingStrategyProvider pricingStrategyProvider,
+        IBeverageFactory beverageFactory,
+        IAddOnDecoratorFactory addOnDecoratorFactory,
+        IOrderEventPublisher orderEventPublisher)
+    {
+        _pricingStrategyProvider = pricingStrategyProvider;
+        _beverageFactory = beverageFactory;
+        _addOnDecoratorFactory = addOnDecoratorFactory;
+        _orderEventPublisher = orderEventPublisher;
+    }
+
+    public Result PlaceOrder(OrderRequest orderRequest)
+    {
+        var result = new Result();
+
+        var pricingStrategyResult = _pricingStrategyProvider.Get(orderRequest.PricingStrategyType);
+        result.AddErrors(pricingStrategyResult.Errors);
+
+        var beverageResult = _beverageFactory.Create(orderRequest.BeverageType);
+        result.AddErrors(beverageResult.Errors);
+
+        if (result.IsFailure)
+        {
+            return result;
+        }
+
+        var finalBeverage = beverageResult.Value!;
+        var pricingStrategy = pricingStrategyResult.Value!;
+        foreach (var addOnSelection in orderRequest.AddOns)
+        {
+            var decoratedBeverageResult = _addOnDecoratorFactory.Create(finalBeverage, addOnSelection);
+            result.AddErrors(decoratedBeverageResult.Errors);
+            if (result.IsFailure)
+            {
+                return result;
+            }
+            finalBeverage = decoratedBeverageResult.Value!;
+        }
+
+        var orderPlaced = new OrderPlaced(finalBeverage, pricingStrategy);
+        _orderEventPublisher.PublishOrderPlaced(orderPlaced);
+
+        return result;
+    }
+}
