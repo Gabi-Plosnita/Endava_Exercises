6b50e70ab4d85bd79cd887dbf5b0726980cb7aad
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2025-11-13 14:53:51 +0200

    Move AddOnDecoratorFactory to Infrastructure layer
    Refactored AddOnDecoratorFactory and its interface from Application to Infrastructure and Domain layers. The factory now uses a registry pattern for decorator creation, improving extensibility and separation of concerns.

---
+++ CafeConsole/Cafe.Domain/Factories/IAddOnDecoratorFactory.cs
@@ -0,0 +1,7 @@
+﻿namespace Cafe.Domain;
+
+public interface IAddOnDecoratorFactory
+{
+    Result<IBeverage> Create(string key, IBeverage inner, params object[] args);
+    IEnumerable<string> Keys { get; }
+}
---
+++ CafeConsole/Cafe.Infrastructure/Factories/AddOnDecoratorFactory.cs
@@ -0,0 +1,35 @@
+﻿using Cafe.Domain;
+
+namespace Cafe.Infrastructure;
+
+public class AddOnDecoratorFactory : IAddOnDecoratorFactory
+{
+    private readonly Dictionary<string, IDecoratorRegistration> _registry;
+
+    public AddOnDecoratorFactory(IEnumerable<IDecoratorRegistration> regs)
+    {
+        _registry = regs.ToDictionary(r => r.Key, StringComparer.OrdinalIgnoreCase);
+    }
+
+    public IEnumerable<string> Keys => _registry.Keys;
+
+    public Result<IBeverage> Create(string key, IBeverage inner, params object[] args)
+    {
+        var result = new Result<IBeverage>();
+        if (!_registry.TryGetValue(key, out var registration))
+        {
+            result.AddError($"Unknown decorator '{key}'");
+            return result;
+        }
+
+        var createDecoratorResult = registration.Create(inner, args);
+        if(createDecoratorResult.IsFailure)
+        {
+            result.AddErrors(createDecoratorResult.Errors);
+            return result;
+        }
+        
+        result.Value = createDecoratorResult.Value;
+        return result;
+    }
+}
