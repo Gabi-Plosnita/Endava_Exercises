0a0e5ffc141cbf308ece12cae2d02b80668880a7
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2025-11-13 21:16:36 +0200

    Add HotChocolateRegistration factory class
    Introduces HotChocolateRegistration implementing IBeverageRegistration for creating and validating HotChocolate beverage instances.

---
+++ CafeConsole/Cafe.Infrastructure/Factories/Beverage/HotChocolateRegistration.cs
@@ -0,0 +1,33 @@
+﻿using Cafe.Domain;
+
+namespace Cafe.Infrastructure;
+
+public class HotChocolateRegistration : IBeverageRegistration
+{
+    private readonly string _name;
+    private readonly decimal _baseCost;
+
+    public HotChocolateRegistration(string name, decimal baseCost)
+    {
+        _name = name;
+        _baseCost = baseCost;
+    }
+
+    public string Key => _name.ToLower();
+
+    public Result<IBeverage> Create()
+    {
+        var result = new Result<IBeverage>();
+        var hotChocolate = new HotChocolate(_name, _baseCost);
+        var validationResult = hotChocolate.Validate();
+
+        if (validationResult.IsFailure)
+        {
+            result.AddErrors(validationResult.Errors);
+            return result;
+        }
+
+        result.Value = hotChocolate;
+        return result;
+    }
+}
