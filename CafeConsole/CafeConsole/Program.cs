44ede0596643b987ff343b8bba948ce67f8a7340
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2025-11-13 17:08:07 +0200

    Add DI setup for Console UI and update Program.cs
    Introduces Microsoft.Extensions.DependencyInjection to the project and adds a new DependencyInjectionConsoleUI class for registering Console UI services. Updates Program.cs to use dependency injection for Menu and application startup.

--- CafeConsole/CafeConsole/Program.cs
+++ CafeConsole/CafeConsole/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Cafe.Application;
+using Cafe.ConsoleUI;
+using Microsoft.Extensions.DependencyInjection;
+
+var services = new ServiceCollection();
+services.RegisterConsoleUI();
+services.RegisterApplication();
+services.RegisterInfrastructure();
+
+var serviceProvider = services.BuildServiceProvider();
+
+var menu = serviceProvider.GetRequiredService<Menu>();
+menu.Run();
