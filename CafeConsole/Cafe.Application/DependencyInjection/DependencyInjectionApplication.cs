4581aef08c39c1df2d80c6682f5fb410aef848e7
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2025-11-13 21:19:21 +0200

    Refactor DI to use registration classes for beverages
    Replaces direct IBeverage singleton registrations with IBeverageRegistration implementations for Espresso, Tea, and Hot Chocolate. Also moves AddOnDecoratorFactory registration to the correct method and ensures both factories are registered. This improves flexibility and maintainability of beverage and add-on registrations.

--- CafeConsole/Cafe.Application/DependencyInjection/DependencyInjectionApplication.cs
+++ CafeConsole/Cafe.Application/DependencyInjection/DependencyInjectionApplication.cs
@@ -13,13 +13,7 @@ public static class DependencyInjectionApplication
         services.AddSingleton<IPricingStrategyProvider, InMemoryPricingStrategyProvider>();
         services.AddSingleton<IOrderEventPublisher, SimpleOrderEventPublisher>();
 
-        /*services.AddSingleton<IBeverage>(sp => new Espresso("Espresso", 2.50m));
-        services.AddSingleton<IBeverage>(sp => new Tea("Tea", 2.00m));
-        services.AddSingleton<IBeverage>(sp => new HotChocolate("Hot Chocolate", 3.00m));*/
-
-        services.AddSingleton<IBeverageFactory, BeverageFactory>();
         services.AddSingleton<IOrderService, OrderService>();
-
         return services;
     }
 
@@ -29,11 +23,16 @@ public static class DependencyInjectionApplication
         services.AddSingleton<IOrderEventSubscriber>(sp => new ConsoleOrderLogger("USD"));
 
         // All theese values like Name and Price can be moved in a json configuration file. I left them here for simplicity.
+        services.AddSingleton<IBeverageRegistration>(sp => new EspressoRegistration("Espresso", 2.50m));
+        services.AddSingleton<IBeverageRegistration>(sp => new TeaRegistration("Tea", 2.00m));
+        services.AddSingleton<IBeverageRegistration>(sp => new HotChocolateRegistration("Hot Chocolate", 3.00m));
+
         services.AddSingleton<IAddOnDecoratorRegistration>(sp => new MilkDecoratorRegistration("Milk", 0.40m));
         services.AddSingleton<IAddOnDecoratorRegistration>(sp => new SyrupDecoratorRegistration("Syrup", 0.50m));
         services.AddSingleton<IAddOnDecoratorRegistration>(sp => new ExtraShotDecoratorRegistration("Extra Shot", 0.80m));
-        services.AddSingleton<IAddOnDecoratorFactory, AddOnDecoratorFactory>();
 
+        services.AddSingleton<IBeverageFactory, BeverageFactory>();
+        services.AddSingleton<IAddOnDecoratorFactory, AddOnDecoratorFactory>();
         return services;
     }
 }
