edf4b070ae71882ca1d7efc02bb60f5117cfc114
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2025-11-14 17:22:29 +0200

    Add unit tests for RegularPricingStrategy
    Introduces RegularPriceStrategyTests to verify that RegularPricingStrategy.Calculate returns the expected result for given input values.

---
+++ CafeConsole/Cafe.UnitTests/Strategies/RegularPriceStrategyTests.cs
@@ -0,0 +1,19 @@
+﻿using Cafe.Domain;
+
+namespace Cafe.UnitTests;
+
+public class RegularPriceStrategyTests
+{
+    [Theory]
+    [InlineData(2.99, 2.99)]
+    [InlineData(10.00, 10.00)]
+
+    public void Calculate_ReturnsExpectedResult_WhenCalled(decimal costBeforeStrategy, decimal expectedAfterStrategy)
+    {
+        var strategy = new RegularPricingStrategy();
+
+        var result = strategy.Calculate(costBeforeStrategy);
+
+        Assert.Equal(expectedAfterStrategy, result);
+    }
+}
