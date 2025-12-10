3a1c847d8485689b523e14e2c001133bf2f56a97
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2025-12-10 10:45:33 +0200

    Add generic EF repository base class
    Introduces EfRepositoryBase to provide common CRUD operations for domain and entity models using Entity Framework Core and AutoMapper. This base class streamlines repository implementations by handling mapping and DbSet access.

---
+++ AirportTool/AirportTool.Infrastructure/Repositories/EfRepositoryBase.cs
@@ -0,0 +1,56 @@
+﻿using AirportTool.Application;
+using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+
+namespace AirportTool.Infrastructure;
+
+public abstract class EfRepositoryBase<TDomain, TEntity, TKey>
+        : IRepository<TDomain, TKey>
+        where TDomain : class
+        where TEntity : class
+{
+    protected readonly AirportDbContext _context;
+    protected readonly IMapper _mapper;
+    protected readonly DbSet<TEntity> _dbSet;
+
+    protected EfRepositoryBase(AirportDbContext context, IMapper mapper)
+    {
+        _context = context;
+        _mapper = mapper;
+        _dbSet = _context.Set<TEntity>();
+    }
+
+    public virtual async Task<TDomain?> GetByIdAsync(TKey id, CancellationToken cancellationToken = default)
+    {
+        var entity = await _dbSet.FindAsync([id], cancellationToken);
+        return _mapper.Map<TDomain>(entity);
+    }
+
+    public virtual async Task<IReadOnlyList<TDomain>> GetAllAsync(CancellationToken cancellationToken = default)
+    {
+        var entities = await _dbSet.AsNoTracking()
+                                   .ToListAsync(cancellationToken);
+
+        return _mapper.Map<IReadOnlyList<TDomain>>(entities);
+    }
+
+    public virtual async Task AddAsync(TDomain domainModel, CancellationToken cancellationToken = default)
+    {
+        var entity = _mapper.Map<TEntity>(domainModel);
+        await _dbSet.AddAsync(entity, cancellationToken);
+    }
+
+    public virtual Task UpdateAsync(TDomain domainModel, CancellationToken cancellationToken = default)
+    {
+        var entity = _mapper.Map<TEntity>(domainModel);
+        _dbSet.Update(entity);
+        return Task.CompletedTask;
+    }
+
+    public virtual Task RemoveAsync(TDomain domainModel, CancellationToken cancellationToken = default)
+    {
+        var entity = _mapper.Map<TEntity>(domainModel);
+        _dbSet.Remove(entity);
+        return Task.CompletedTask;
+    }
+}
