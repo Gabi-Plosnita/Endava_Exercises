a0b68b407ac17c746166be44523f65a4a10d2355
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2025-12-10 10:30:19 +0200

    Add generic IRepository interface
    Introduces a generic IRepository interface for basic CRUD operations with asynchronous methods, supporting domain models and keys. This will help standardize data access patterns across the application.

---
+++ AirportTool/AirportTool.Application/Interfaces/Persistance/IRepository.cs
@@ -0,0 +1,14 @@
+﻿namespace AirportTool.Application;
+
+public interface IRepository<TDomain, TKey> where TDomain : class
+{
+    Task<TDomain?> GetByIdAsync(TKey id, CancellationToken cancellationToken = default);
+
+    Task<IReadOnlyList<TDomain>> GetAllAsync(CancellationToken cancellationToken = default);
+
+    Task AddAsync(TDomain domainModel, CancellationToken cancellationToken = default);
+
+    Task UpdateAsync(TDomain domainModel, CancellationToken cancellationToken = default);
+
+    Task RemoveAsync(TDomain domainModel, CancellationToken cancellationToken = default);
+}
