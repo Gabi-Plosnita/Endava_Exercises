8d490b89e8ab1fe916de7dd1626de73f4a2e359b
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2025-12-11 15:13:10 +0200

    Add PagedQueryDto and FlightFilterDto classes
    Introduces PagedQueryDto for pagination parameters and FlightFilterDto for filtering flights by origin, destination, and date. FlightFilterDto inherits from PagedQueryDto to support paginated flight queries.

---
+++ AirportTool/AirportTool.Application/Dtos/Common/PagedQueryDto.cs
@@ -0,0 +1,7 @@
+﻿namespace AirportTool.Application;
+
+public class PagedQueryDto
+{
+    public int PageIndex { get; init; } = 0;
+    public int PageSize { get; init; } = 10;
+}
---
+++ AirportTool/AirportTool.Application/Dtos/Flight/FlightFilterDto.cs
@@ -0,0 +1,8 @@
+﻿namespace AirportTool.Application;
+
+public class FlightFilterDto : PagedQueryDto
+{
+    public string? OriginIata { get; init; }
+    public string? DestinationIata { get; init; }
+    public DateOnly? Date { get; init; }
+}
