088ad93d36349e854aa9708cdb7671c17df1d420
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2025-12-12 15:27:09 +0200

    Add FlightScheduleDetailsDto for flight schedule data
    Introduces the FlightScheduleDetailsDto class to encapsulate detailed information about flight schedules, including flight, airline, origin, destination, schedule times, status, aircraft, and gate code. This DTO will be used for transferring flight schedule details within the application.

---
+++ AirportTool/AirportTool.Application/Dtos/Flight/FlightScheduleDetailsDto.cs
@@ -0,0 +1,29 @@
+﻿using AirportTool.Domain;
+
+namespace AirportTool.Application;
+
+public class FlightScheduleDetailsDto
+{
+    public int FlightScheduleId { get; set; }
+    public int FlightId { get; set; }
+    public string FlightNumber { get; set; } = null!;
+
+    public string AirlineIata { get; set; } = null!;
+    public string AirlineName { get; set; } = null!;
+
+    public string OriginIata { get; set; } = null!;
+    public string OriginName { get; set; } = null!;
+
+    public string DestinationIata { get; set; } = null!;
+    public string DestinationName { get; set; } = null!;
+
+    public DateTime ScheduledDepartureUtc { get; set; }
+    public DateTime ScheduledArrivalUtc { get; set; }
+
+    public FlightScheduleStatus Status { get; set; }
+
+    public string? AssignedAircraftTail { get; set; }
+    public string? DefaultAircraftTail { get; set; }
+
+    public string? GateCode { get; set; }
+}
