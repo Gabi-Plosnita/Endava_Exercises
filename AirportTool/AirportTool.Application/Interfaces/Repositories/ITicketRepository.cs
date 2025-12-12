db3dbc0f5ef9d0201bf81822c860a8c242f28a71
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2025-12-12 10:10:32 +0200

    Add Ticket repository interface and implementation
    Introduces ITicketRepository interface and its implementation TicketRepository. Adds method to retrieve tickets by flight schedule ID, supporting asynchronous operations.

---
+++ AirportTool/AirportTool.Application/Interfaces/Repositories/ITicketRepository.cs
@@ -0,0 +1,10 @@
+﻿using AirportTool.Domain;
+
+namespace AirportTool.Application;
+
+public interface ITicketRepository : IRepository<Ticket, long>
+{
+    Task<IReadOnlyCollection<Ticket>> GetTicketsByFlightScheduleIdAsync(
+        int flightScheduleId,
+        CancellationToken cancellationToken = default);
+}
---
+++ AirportTool/AirportTool.Infrastructure/Repositories/TicketRepository.cs
@@ -0,0 +1,25 @@
+﻿using AirportTool.Application;
+using AirportTool.Domain;
+using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+
+namespace AirportTool.Infrastructure;
+
+public class TicketRepository : EfRepositoryBase<Ticket, TicketDb, long>, ITicketRepository
+{
+    public TicketRepository(AirportDbContext context, IMapper mapper) : base(context, mapper)
+    {
+    }
+
+    public async Task<IReadOnlyCollection<Ticket>> GetTicketsByFlightScheduleIdAsync(
+        int flightScheduleId,
+        CancellationToken cancellationToken = default)
+    {
+        var tickets = await _context.Tickets
+                                    .AsNoTracking()
+                                    .Where(t => t.FlightScheduleId == flightScheduleId)
+                                    .ToListAsync(cancellationToken);
+
+        return _mapper.Map<IReadOnlyCollection<Ticket>>(tickets);
+    }
+}
