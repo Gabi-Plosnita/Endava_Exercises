e106accf4a7364ffdebbed5761a62eb3fcf5edfc
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2025-12-15 17:22:18 +0200

    Add Airport repository interface and implementation
    Introduces IAirportRepository interface with a method to fetch airports by IATA code, and provides its implementation in AirportRepository using Entity Framework. This supports querying airports by their IATA code asynchronously.

---
+++ AirportTool/AirportTool.Infrastructure/Repositories/AirportRepository.cs
@@ -0,0 +1,23 @@
+﻿using AirportTool.Application;
+using AirportTool.Domain;
+using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+
+namespace AirportTool.Infrastructure;
+
+public class AirportRepository : EfRepositoryBase<Airport, AirportDb, int>, IAirportRepository
+{
+    public AirportRepository(AirportDbContext context, IMapper mapper) : base(context, mapper)
+    {
+    }
+
+    public async Task<Airport?> GetByIataCodeAsync(string iataCode, CancellationToken cancellationToken = default)
+    {
+        var airportDb = await _context.Airports
+                                      .AsNoTracking()
+                                      .SingleOrDefaultAsync(a => a.Iatacode == iataCode, cancellationToken);
+
+        var airport = _mapper.Map<Airport>(airportDb);
+        return airport;
+    }
+}
