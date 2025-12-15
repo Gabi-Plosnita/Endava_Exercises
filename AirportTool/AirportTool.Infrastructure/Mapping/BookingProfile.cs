2e33e267677b3d2217e5feaeb1f9ae75b37d5a8c
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2025-12-15 11:25:32 +0200

    Add BookingProfile for AutoMapper configuration
    Introduces BookingProfile to map between Booking and BookingDb entities using AutoMapper, enabling bidirectional mapping.

---
+++ AirportTool/AirportTool.Infrastructure/Mapping/BookingProfile.cs
@@ -0,0 +1,12 @@
+﻿using AirportTool.Domain;
+using AutoMapper;
+
+namespace AirportTool.Infrastructure;
+
+public class BookingProfile : Profile
+{
+    public BookingProfile()
+    {
+        CreateMap<Booking, BookingDb>().ReverseMap();
+    }
+}
