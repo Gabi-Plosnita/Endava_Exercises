91f4ba3dc1190ae414884ee04e16566dafc751ad
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2025-12-15 12:15:13 +0200

    Add FlightScheduleProfile for AutoMapper mapping
    Introduces FlightScheduleProfile to configure AutoMapper mappings between FlightSchedule and FlightScheduleDb entities, enabling bidirectional mapping.

---
+++ AirportTool/AirportTool.Infrastructure/Mapping/FlightScheduleProfile.cs
@@ -0,0 +1,12 @@
+﻿using AirportTool.Domain;
+using AutoMapper;
+
+namespace AirportTool.Infrastructure;
+
+public class FlightScheduleProfile : Profile
+{
+    public FlightScheduleProfile()
+    {
+        CreateMap<FlightSchedule, FlightScheduleDb>().ReverseMap();
+    }
+}
