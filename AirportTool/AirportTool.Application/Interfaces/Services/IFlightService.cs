e89d565ca80e2208a72feb0939b86f1fa32f75fe
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2025-12-15 16:08:44 +0200

    Add CRUD method signatures to IFlightService
    Introduced asynchronous methods for retrieving, creating, updating, and deleting flights in the IFlightService interface to support full CRUD operations.

--- AirportTool/AirportTool.Application/Interfaces/Services/IFlightService.cs
+++ AirportTool/AirportTool.Application/Interfaces/Services/IFlightService.cs
@@ -2,4 +2,11 @@
 
 public interface IFlightService
 {
+    Task<Result<GetFlightDto?>> GetByIdAsync(int flightId, CancellationToken cancellationToken);
+
+    Task<Result<GetFlightDto?>> CreateAsync(CreateFlightDto dto, CancellationToken cancellationToken);
+
+    Task<Result> UpdateAsync(int flightId, CreateFlightDto dto, CancellationToken cancellationToken);
+
+    Task<Result> DeleteByIdAsync(int flightId, CancellationToken cancellationToken);
 }
