47fc1eabcbbff4dbece05b66e647c05682b1ad0c
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2025-12-15 16:22:25 +0200

    Add FlightService implementation and update IFlightService
    Introduces the FlightService class with basic CRUD method stubs and a working DeleteByIdAsync implementation. Updates IFlightService to change the return type of GetByIdAsync to match the new implementation.

--- AirportTool/AirportTool.Application/Interfaces/Services/IFlightService.cs
+++ AirportTool/AirportTool.Application/Interfaces/Services/IFlightService.cs
@@ -2,7 +2,7 @@
 
 public interface IFlightService
 {
-    Task<Result<GetFlightDto?>> GetByIdAsync(int flightId, CancellationToken cancellationToken);
+    Task<GetFlightDto?> GetByIdAsync(int flightId, CancellationToken cancellationToken);
 
     Task<Result<GetFlightDto?>> CreateAsync(CreateFlightDto dto, CancellationToken cancellationToken);
 
---
+++ AirportTool/AirportTool.Application/Services/FlightService.cs
@@ -0,0 +1,48 @@
+﻿using AutoMapper;
+
+namespace AirportTool.Application;
+
+public class FlightService : IFlightService
+{
+    private readonly IUnitOfWork _unitOfWork;
+    private readonly IMapper _mapper;
+
+    public FlightService(IUnitOfWork unitOfWork, IMapper mapper)
+    {
+        _unitOfWork = unitOfWork;
+        _mapper = mapper;
+    }
+
+    public async Task<GetFlightDto?> GetByIdAsync(int flightId, CancellationToken cancellationToken)
+    {
+        var flight = await _unitOfWork.Flights.GetByIdAsync(flightId, cancellationToken);
+        var getFlightDto = _mapper.Map<GetFlightDto>(flight);
+        return getFlightDto;
+    }
+
+    public async Task<Result<GetFlightDto?>> CreateAsync(CreateFlightDto dto, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<Result> UpdateAsync(int flightId, CreateFlightDto dto, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<Result> DeleteByIdAsync(int flightId, CancellationToken cancellationToken)
+    {
+        var result = new Result();
+
+        var flight = await _unitOfWork.Flights.GetByIdAsync(flightId, cancellationToken);
+        if (flight == null)
+        {
+            result.AddError($"Flight with ID {flightId} not found.");
+            return result;
+        }
+
+        await _unitOfWork.Flights.RemoveAsync(flight, cancellationToken);
+        await _unitOfWork.SaveChangesAsync(cancellationToken);
+        return result;
+    }
+}
