1e0dcf48634a8650382ff888a66fd58ba96b7a3a
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2025-12-17 12:39:52 +0200

    Add IAircraftService interface definition
    Introduces the IAircraftService interface with methods for CRUD operations and filtering of aircraft entities. This provides a contract for implementing aircraft-related service logic in the application layer.

---
+++ AirportTool/AirportTool.Application/Interfaces/Services/IAircraftService.cs
@@ -0,0 +1,14 @@
+﻿namespace AirportTool.Application;
+
+public interface IAircraftService
+{
+    Task<Result<GetAircraftDto?>> GetByIdAsync(int id, CancellationToken cancellationToken);
+
+    Task<Result<PagedResult<GetAircraftDto>>> GetByFilterAsync(AircraftFilterDto dto, CancellationToken cancellationToken);
+
+    Task<Result<GetAircraftDto?>> CreateAsync(CreateAircraftDto dto, CancellationToken cancellationToken);
+
+    Task<Result> UpdateAsync(int id, UpdateAircraftDto dto, CancellationToken cancellationToken);
+
+    Task<Result> DeleteByIdAsync(int id, CancellationToken cancellationToken);
+}
