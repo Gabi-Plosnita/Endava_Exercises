c688644be5e522f515ffa18c77dbf39bc2166a4e
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2025-12-17 09:17:39 +0200

    Add Error class and ErrorType enum
    Introduces an Error class with Message and Type properties, and defines an ErrorType enum with NotFound and Validation values for error handling.

---
+++ AirportTool/AirportTool.Application/Result/Error.cs
@@ -0,0 +1,14 @@
+﻿namespace AirportTool.Application;
+
+public class Error
+{
+    public string Message { get; set; } = null!;
+
+    public ErrorType Type { get; set; }
+}
+
+public enum ErrorType
+{
+    NotFound,
+    Validation,
+}
