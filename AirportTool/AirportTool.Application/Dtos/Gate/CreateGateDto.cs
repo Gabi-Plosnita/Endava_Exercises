95ecdfb37befa0e4ab75fa3c1f2138c8d0e2e72e
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2025-12-18 15:20:28 +0200

    Add DTOs for Gate entity operations
    Introduced CreateGateDto, GetGateDto, and UpdateGateDto classes to support creation, retrieval, and update operations for Gate entities in the application layer.

---
+++ AirportTool/AirportTool.Application/Dtos/Gate/CreateGateDto.cs
@@ -0,0 +1,8 @@
+﻿namespace AirportTool.Application;
+
+public class CreateGateDto
+{
+    public int AirportId { get; set; }
+
+    public string Code { get; set; } = null!;
+}
---
+++ AirportTool/AirportTool.Application/Dtos/Gate/GetGateDto.cs
@@ -0,0 +1,12 @@
+﻿namespace AirportTool.Application;
+
+public class GetGateDto
+{
+    public int GateId { get; set; }
+
+    public string AirportIataCode { get; set; } = null!;
+
+    public string AirportName { get; set; } = null!;
+
+    public string Code { get; set; } = null!;
+}
