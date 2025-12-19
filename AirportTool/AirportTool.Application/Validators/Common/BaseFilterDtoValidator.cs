9cdeb9756c34a466158acdb63f46147cdbf037a8
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2025-12-19 11:11:47 +0200

    Add BaseFilterDtoValidator for filter DTO validation
    Introduces BaseFilterDtoValidator to validate BaseFilterDto instances, ensuring PageIndex is non-negative and PageSize is between 1 and 100. Returns validation errors if constraints are not met.

---
+++ AirportTool/AirportTool.Application/Validators/Common/BaseFilterDtoValidator.cs
@@ -0,0 +1,26 @@
+﻿namespace AirportTool.Application;
+
+public class BaseFilterDtoValidator : BaseValidator, IValidator<BaseFilterDto>
+{
+    public Result Validate(BaseFilterDto instance)
+    {
+        var result = new Result();
+        if (instance.PageIndex < 0)
+        {   
+            result.AddError(new Error
+            {
+                Message = "PageIndex must be greater than or equal to 0.",
+                Type = ErrorType.Validation
+            });
+        }
+        if(instance.PageSize <= 0 || instance.PageSize > 100)
+        {
+            result.AddError(new Error
+            {
+                Message = "PageSize must be between 1 and 100",
+                Type = ErrorType.Validation
+            });
+        }
+        return result;
+    }
+}
