ac44ab34f3fd9430a29de3bf87cddf93af47f4e7
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2025-12-19 11:14:41 +0200

    Add GetByFilterAsync to IFlightSchedulesService
    Introduced a new method GetByFilterAsync to the IFlightSchedulesService interface for retrieving paged flight schedules based on filter criteria.

--- AirportTool/AirportTool.Application/Interfaces/Services/IFlightSchedulesService.cs
+++ AirportTool/AirportTool.Application/Interfaces/Services/IFlightSchedulesService.cs
@@ -4,6 +4,8 @@ public interface IFlightSchedulesService
 {
     Task<GetFlightScheduleDto?> GetByIdAsync(int id, CancellationToken cancellationToken);
 
+    Task<Result<PagedResult<FlightScheduleSearchDto>>> GetByFilterAsync(FlightFilterDto dto, CancellationToken cancellationToken);
+
     Task<Result<IReadOnlyList<DailyFlightStatsDto>>> GetDailyStatsAsync(DateTime startUtc, DateTime endUtc, CancellationToken cancellationToken);
 
     Task<Result<GetFlightScheduleDto?>> CreateAsync(UpsertFlightScheduleDto dto, CancellationToken cancellationToken);
