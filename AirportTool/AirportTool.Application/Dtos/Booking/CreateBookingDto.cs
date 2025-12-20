454ceaec80b519909bd1824b594b98eb5eff9c54
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2025-12-20 16:55:07 +0200

    Add DTOs for booking creation and retrieval
    Introduces CreateBookingDto and GetBookingDto classes to support booking creation and retrieval operations. These DTOs encapsulate relevant booking data such as ticket ID, passenger details, quantity, confirmation code, total amount, and status.

---
+++ AirportTool/AirportTool.Application/Dtos/Booking/CreateBookingDto.cs
@@ -0,0 +1,12 @@
+﻿namespace AirportTool.Application;
+
+public class CreateBookingDto
+{
+    public long TicketId { get; set; }
+
+    public string PassengerFullName { get; set; } = null!;
+
+    public string PassengerEmail { get; set; } = null!;
+
+    public int Quantity { get; set; }
+}
---
+++ AirportTool/AirportTool.Application/Dtos/Booking/GetBookingDto.cs
@@ -0,0 +1,20 @@
+﻿using AirportTool.Domain;
+
+namespace AirportTool.Application;
+
+public class GetBookingDto
+{
+    public long TicketId { get; set; }
+
+    public string PassengerFullName { get; set; } = null!;
+
+    public string PassengerEmail { get; set; } = null!;
+
+    public string ConfirmationCode { get; set; } = null!;
+
+    public int Quantity { get; set; }
+
+    public decimal TotalAmount { get; set; }
+
+    public BookingStatus Status { get; set; }
+}
