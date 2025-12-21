180b29a028c91b79bc9ecf6f86a3c7b25edd35d4
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2025-12-21 14:38:52 +0200

    Rename 'code' parameter to 'confirmationCode' in IBookingService
    Updated method signatures in IBookingService to use 'confirmationCode' instead of 'code' for clarity and consistency.

--- AirportTool/AirportTool.Application/Interfaces/Services/IBookingService.cs
+++ AirportTool/AirportTool.Application/Interfaces/Services/IBookingService.cs
@@ -2,9 +2,9 @@
 
 public interface IBookingService
 {
-    public Task<GetBookingDto?> GetBookingByCodeAsync(string code, CancellationToken cancellationToken);
+    public Task<GetBookingDto?> GetBookingByCodeAsync(string confirmationCode, CancellationToken cancellationToken);
 
     public Task<Result<GetBookingDto?>> CreateBookingAsync(CreateBookingDto createBookingDto, CancellationToken cancellationToken);
 
-    public Task<Result> CancelBookingAsync(string code, CancellationToken cancellationToken);
+    public Task<Result> CancelBookingAsync(string confirmationCode, CancellationToken cancellationToken);
 }
